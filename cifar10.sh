2ccf0a1885d622e6b285cde3bef208e4643a7e5e
apuaaChen <chenzd15thu@gmail.com>
2019-10-31 14:13:24 -0700

    Add codes for CIFAR-10

--- cifar10.sh
+++ cifar10.sh
@@ -1,2 +1,2 @@
-DATADIR = $1
-python -m cifar.explist --dataset_dir=$DATADIR --type=initialization --repeat=1 --start=0 --gpu=1
+echo "Data dir: $1"
+python -m cifar.exp_list --dataset_dir=$1 --type=initialization --repeat=1 --start=0 --gpu=1
