0866b673f8fbc9039b2bbb02a174b69647d5403a
apuaaChen <chenzd15thu@gmail.com>
2019-10-31 21:59:49 -0700

    add bash script

--- cifar10.sh
+++ cifar10.sh
@@ -1,2 +1,3 @@
 echo "Data dir: $1"
-python -m cifar.exp_list --dataset_dir=$1 --type=initialization --repeat=1 --start=0 --gpu=1
+echo "Exp: $2"
+python -m cifar.exp_list --dataset_dir=$1 --type=$2 --repeat=1 --start=0 --gpu=1
