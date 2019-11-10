480c98dd24534eada7911ed3c707f21990ab9eb7
apuaaChen <chenzd15thu@gmail.com>
2019-11-10 14:00:05 -0800

    minor modification

--- cifar10.sh
+++ cifar10.sh
@@ -1,3 +1,6 @@
 echo "Data dir: $1"
 echo "Exp: $2"
-python -m cifar.exp_list --dataset_dir=$1 --type=$2 --repeat=1 --start=0 --gpu=1
+echo "GPU: $3"
+echo "repeat: $4"
+echo "start: $5"
+python -m cifar.exp_list --dataset_dir=$1 --type=$2 --repeat=$4 --start=$5 --gpu=$3
